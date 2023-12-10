dfdfd4e5003fa1b93df8f096ba7808e4345a3ce0
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-10 21:52:03 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from "./view/common/Navbar/Navbar";
+import {MainContent} from "./view/common/MainContent/MainContent";
+import {Footer} from "./view/common/Footer/Footer";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Navbar/>
+        <MainContent/>
+        <Footer/>
+      </BrowserRouter>
   );
 }
 
