7fc404bf253a2bf0d267e7da65a16aee9e6ee346
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-12 11:31:30 +0530

    Create Menu.tsx

---
+++ src/view/pages/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+export class Menu extends Component {
+    render() {
+        return (
+            <>
+                <h1 className="text-4xl text-center
+                               text-tertiary">
+                    This is Menu Component!</h1>
+            </>
+        );
+    }
+}
