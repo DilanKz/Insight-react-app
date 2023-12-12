895ac3ca41c8fa3058c61600fa8219ebdb498a0e
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-12 11:31:36 +0530

    Update MainContent.tsx

--- src/view/common/MainContent/MainContent.tsx
+++ src/view/common/MainContent/MainContent.tsx
@@ -3,17 +3,18 @@ import {Route, Routes} from "react-router-dom";
 import {Home} from "../../pages/Home/Home";
 import {About} from "../../pages/About/About";
 import {Contact} from "../../pages/Contact/Contact";
+import {Menu} from "../../pages/Menu/Menu";
 
 export class MainContent extends Component {
     render() {
         return (
-            <div className="pt-28 pb-28 mx-auto bg-amber-100">
-                {/*<h1 className="text-4xl text-center text-tertiary">*/}
-                {/*    This is Main Content!</h1>*/}
+            <div className="min-h-[94vh] flex items-center justify-center
+             mx-auto bg-white overflow-x-hidden">
 
                 <Routes>
                     <Route path="/" Component={Home}></Route>
                     <Route path="/about" Component={About}></Route>
+                    <Route path="/menu" Component={Menu}></Route>
                     <Route path="/contact" Component={Contact}></Route>
                 </Routes>
             </div>
