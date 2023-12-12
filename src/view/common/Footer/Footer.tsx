8dd6fe0f37da75c81bb804dc8e194615995a5184
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-12 11:31:26 +0530

    Update Footer.tsx

--- src/view/common/Footer/Footer.tsx
+++ src/view/common/Footer/Footer.tsx
@@ -5,19 +5,16 @@ import {Link} from "react-router-dom";
 export class Footer extends Component {
     render() {
         return (
-            <div className="p-2 bg-[#444544] flex
-                            justify-center">
+            <div className="p-2 bg-gray-300 flex
+                            justify-center items-center">
 
-                <p className="text-[11px] text-[#e6f0e6]
-                              pr-2 pt-1 hover:text-green-400">
+                <p className="text-[11px] text-gray-800
+                              pr-2 pt-1 hover:text-green-400 ">
                     Copyright @ 2023</p>
 
                 <Link to="/">
                     <div className="flex">
-                        <h1 className="text-1xl text-secondary">
-                            Organic Shop</h1>
-                        <img className="h-5 w-5 top-0 ml-1"
-                             src={logo} alt=""/>
+                        <h1 className="text-1xl text-yellow-700">Quick-bite</h1>
                     </div>
                 </Link>
             </div>
