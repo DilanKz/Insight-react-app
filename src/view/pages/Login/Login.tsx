cc9bd85e7cdf2fd121abf6932ba6266e6a417877
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-16 10:21:05 +0530

    Create Login.tsx

---
+++ src/view/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export class Login extends Component {
+    render() {
+        return (
+            <div className="h-screen flex justify-center items-center">
+                <h1 className="text-4xl text-center text-tertiary">
+                    This is Login!</h1>
+            </div>
+        );
+    }
+}
