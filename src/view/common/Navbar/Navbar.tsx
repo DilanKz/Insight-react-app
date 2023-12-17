ad35edb38566f50643cc41f763a455aeffd77f74
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-17 19:12:14 +0530

    Update Navbar.tsx

--- src/view/common/Navbar/Navbar.tsx
+++ src/view/common/Navbar/Navbar.tsx
@@ -1,63 +1,84 @@
-import {Component, useState} from "react";
-import {Link} from "react-router-dom";
+import React, { Component, MouseEvent } from 'react';
+import { Link } from 'react-router-dom';
 
+interface NavbarProps {}
 
-export const Navbar = () => {
+interface NavbarState {
+    open: boolean;
+}
 
-    let [open,setOpen]=useState(false)
+export class Navbar extends Component<NavbarProps, NavbarState> {
+    constructor(props: NavbarProps) {
+        super(props);
+        this.state = {
+            open: false,
+        };
+    }
 
-    return (
-        <div className="w-full fixed top-0 left-0 bg-white">
+    toggleMenu = () => {
+        this.setState((prevState) => ({ open: !prevState.open }));
+    };
 
-            <div className="md:flex justify-between items-center py-4">
+    render() {
+        const { open } = this.state;
 
-                <div className="ml-7 font-light text-2xl cursor-pointer flex item-center">
+        return (
+            <div className="w-full fixed top-0 left-0 bg-white">
+                <div className="md:flex justify-between items-center py-4">
+                    <div className="ml-7 font-light text-2xl cursor-pointer flex item-center">
+                        <h1 className="text-2xl">
+                            <Link className="font-sans pl-4" to="/">
+                                Insight
+                            </Link>
+                        </h1>
+                    </div>
 
-                    <h1 className="text-2xl">
+                    <div
+                        onClick={this.toggleMenu}
+                        className="absolute text-3xl right-8 top-4 cursor-pointer md:hidden"
+                    >
+                        {open ? 'M' : 'C'}
+                    </div>
 
-                        <Link className="font-sans" to="/">Insight</Link>
+                    <ul
+                        onClick={this.toggleMenu}
+                        className={`absolute md:static md:flex item-center md:pb-0 left-0 w-full md:w-auto transition-all duration-300 ease-in backdrop-blur bg-white ${
+                            open ? 'top-15' : 'top-[-490px]'
+                        }`}
+                    >
+                        <li className="md:ml-12 ml-8 text-[15px] md:my-0 my-5 flex items-center">
+                            <Link
+                                className="text-gray-800 hover:text-gray-500 duration-75 pb-1 hover:border-b-4 border-[#526D82]"
+                                to="/"
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="md:ml-12 ml-8 text-[15px] md:my-0 my-5 flex items-center">
+                            <Link
+                                className="text-gray-800 hover:text-gray-500 duration-75 pb-1 hover:border-b-4 border-[#526D82]"
+                                to="/contact"
+                            >
+                                Blogs
+                            </Link>
+                        </li>
+                        <li className="md:mr-32 md:ml-12 ml-8 text-[15px] md:my-0 my-5 flex items-center">
+                            <Link
+                                className="text-gray-800 hover:text-gray-500 duration-75 pb-1 hover:border-b-4 border-[#526D82]"
+                                to="/menu"
+                            >
+                                Categories
+                            </Link>
+                        </li>
 
-                    </h1>
-
+                        <button className="md:pb-1 mb-2 bg-[#27374D] text-white px-3 py-1 text-[16px] rounded-[6px] sm:ml-7 ml-7 mr-3">
+                            <Link className="" to="/sign">
+                                Get Started
+                            </Link>
+                        </button>
+                    </ul>
                 </div>
-
-                <div onClick={()=>setOpen(!open)} className="absolute text-3xl right-8 top-4
-                    cursor-pointer md:hidden">
-                    {open ? 'M':'C'}
-                </div>
-
-                <ul onClick={()=>setOpen(!open)} className={"absolute md:static md:flex item-center md:pb-0" +
-                    " left-0 w-full md:w-auto transition-all duration-300 ease-in backdrop-blur bg-white" +
-                    ` ${open ? 'top-15':'top-[-490px]' } `}>
-
-                    <li className="md:ml-12 ml-8 text-[15px] md:my-0 my-5 flex items-center">
-                        <Link className="text-gray-800 hover:text-gray-500 duration-75 pb-1
-                            hover:border-b-4 border-[#526D82]"
-                              to="/">Home</Link>
-                    </li>
-                    <li className="md:ml-12 ml-8 text-[15px] md:my-0 my-5 flex items-center">
-                        <Link className="text-gray-800 hover:text-gray-500 duration-75 pb-1
-                            hover:border-b-4 border-[#526D82]"
-                              to="/contact">Blogs</Link>
-                    </li>
-                    <li className="md:mr-32 md:ml-12 ml-8 text-[15px] md:my-0 my-5 flex items-center">
-                        <Link className="text-gray-800 hover:text-gray-500 duration-75 pb-1
-                            hover:border-b-4 border-[#526D82]"
-                              to="/menu">Categories</Link>
-                    </li>
-
-                    <button className="md:pb-1 mb-2 bg-[#27374D] text-white px-3 py-1 text-[16px] rounded-[6px] sm:ml-7 ml-7 mr-3">
-
-                        <Link className="" to="/sign">Get Started</Link>
-
-                    </button>
-
-                </ul>
-
-
             </div>
-
-        </div>
-    );
-
+        );
+    }
 }
