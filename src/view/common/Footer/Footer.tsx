61269460db68585fb9eb911ae395cf30318d13e2
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-18 15:13:05 +0530

    Update Footer.tsx

--- src/view/common/Footer/Footer.tsx
+++ src/view/common/Footer/Footer.tsx
@@ -1,22 +1,31 @@
 import {Component} from "react";
-import logo from '../../../images/icon.png'
-import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faInstagramSquare,
+    faSquareFacebook,
+    faSquareXTwitter,
+    faWhatsappSquare
+} from "@fortawesome/free-brands-svg-icons";
 
 export class Footer extends Component {
     render() {
         return (
-            <div className="p-2 bg-gray-300 flex
-                            justify-center items-center">
-
-                <p className="text-[11px] text-gray-800
-                              pr-2 pt-1 hover:text-green-400 ">
-                    Copyright @ 2023</p>
-
-                <Link to="/">
-                    <div className="flex">
-                        <h1 className="text-1xl text-white">Quick-bite</h1>
-                    </div>
-                </Link>
+            <div className="px-4 py-6 md:flex md:items-center md:justify-between bg-tertiary">
+                <span className="text-sm text-gray-500 sm:text-center">© 2023 Insight. All Rights Reserved.</span>
+                <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
+                    <a href="#" className="text-primary hover:text-secondary">
+                        <FontAwesomeIcon icon={faSquareFacebook}></FontAwesomeIcon>
+                    </a>
+                    <a href="#" className="text-primary hover:text-secondary">
+                        <FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon>
+                    </a>
+                    <a href="#" className="text-primary hover:text-secondary">
+                        <FontAwesomeIcon icon={faSquareXTwitter}></FontAwesomeIcon>
+                    </a>
+                    <a href="#" className="text-primary hover:text-secondary">
+                        <FontAwesomeIcon icon={faWhatsappSquare}></FontAwesomeIcon>
+                    </a>
+                </div>
             </div>
         );
     }
