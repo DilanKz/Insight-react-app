01045c988e48c01010c9779c462437885d2acaab
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-23 22:15:24 +0530

    Update Article.tsx

--- src/view/common/Article/Article.tsx
+++ src/view/common/Article/Article.tsx
@@ -1,7 +1,15 @@
 import {Component} from "react";
 
-export class Article extends Component {
+interface ArticleProps {
+    data:any;
+}
+
+export class Article extends Component<ArticleProps> {
+
     render() {
+
+        const {data}=this.props;
+
         return (
             <div className="w-[350px] h-fit pb-5 justify-center flex flex-wrap pt-6">
                 <div className="w-[330px] h-[400px] rounded-2xl">
@@ -13,23 +21,22 @@ export class Article extends Component {
                 </div>
                 <div className="w-[330px] min-h-8 flex gap-x-3 text-xs mt-1">
 
-                                <span className="bg-gray-200 px-2 py-0.5 h-[1.4rem]
-                                    rounded-full font-light">
-                                        Sport
+                    {
+                        data.tags.map((tag:any)=> (
+
+                            <span className="bg-gray-200 flex justify-center items-center p-2 h-[1.4rem]
+                                    rounded-full font-light hover:cursor-pointer">
+                                        {tag}
                                     </span>
 
-                    <span className="bg-gray-200 px-2 py-0.5 h-[1.4rem]
-                                    rounded-full font-light">
-                                        Wrestling
-                                    </span>
+                        ))
+                    }
 
                 </div>
                 <div className="w-[330px] h-16 mt-3 flex flex-wrap">
 
-                    <h3 className="text-2xl cursor-pointer hover:underline">Roman reigns retired
-                        today in
-                        RAW while crying</h3>
-
+                    <h3 className="text-2xl cursor-pointer hover:underline">
+                    {data.title}</h3>
                 </div>
 
             </div>
