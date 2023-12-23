e426ad3963fb3c3a9894d8b9dde18e8ed935015c
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-23 22:15:28 +0530

    Update Navbar.tsx

--- src/view/common/Navbar/Navbar.tsx
+++ src/view/common/Navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import React, { Component, MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faXmark} from "@fortawesome/free-solid-svg-icons";
 
 interface NavbarProps {}
 
@@ -33,19 +35,14 @@ export class Navbar extends Component<NavbarProps, NavbarState> {
                         </h1>
                     </div>
 
-                    <div
-                        onClick={this.toggleMenu}
-                        className="absolute text-3xl right-8 top-4 cursor-pointer md:hidden"
-                    >
-                        {open ? 'M' : 'C'}
+                    <div onClick={this.toggleMenu}
+                        className="absolute text-3xl right-8 top-4 cursor-pointer md:hidden">
+                        {open ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />}
                     </div>
 
-                    <ul
-                        onClick={this.toggleMenu}
-                        className={`absolute md:static md:flex item-center md:pb-0 left-0 w-full md:w-auto transition-all duration-300 ease-in backdrop-blur bg-tertiary ${
-                            open ? 'top-15' : 'top-[-490px]'
-                        }`}
-                    >
+                    <ul onClick={this.toggleMenu}
+                        className={`absolute md:static md:flex item-center md:pb-0 left-0 w-full md:w-auto pl-4 transition-all duration-300 ease-in backdrop-blur bg-tertiary 
+                        ${open ? 'top-15' : 'top-[-490px]'}`}>
                         <li className="md:ml-12 ml-8 text-[15px] md:my-0 my-5 flex items-center">
                             <Link
                                 className="text-gray-800 hover:text-gray-500 duration-75 pb-1 hover:border-b-4 border-[#526D82]"
@@ -71,11 +68,11 @@ export class Navbar extends Component<NavbarProps, NavbarState> {
                             </Link>
                         </li>
 
-                        <button className="md:pb-1 mb-2 bg-[#27374D] text-white px-3 py-1 text-[16px] rounded-[6px] sm:ml-7 ml-7 mr-3">
-                            <Link className="" to="/sign">
+                        <Link className="" to="/sign">
+                            <button className="md:pb-1 mb-2 bg-[#27374D] text-white px-3 py-1 text-[16px] rounded-[6px] sm:ml-7 ml-7 mr-3">
                                 Get Started
-                            </Link>
-                        </button>
+                            </button>
+                        </Link>
                     </ul>
                 </div>
             </div>
