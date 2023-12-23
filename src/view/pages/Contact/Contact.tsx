0d49dfea19571ad26bc07d1e4f2e42e3b8dc3430
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-23 22:15:31 +0530

    Update Contact.tsx

--- src/view/pages/Contact/Contact.tsx
+++ src/view/pages/Contact/Contact.tsx
@@ -3,11 +3,13 @@ import {Component} from "react";
 export class Contact extends Component {
     render() {
         return (
-            <>
-                <h1 className="text-4xl text-center
-                               text-tertiary">
-                    This is Contact Component!</h1>
-            </>
+            <div className="w-screen mt-[6rem] pl-10">
+
+                <div className="w-screen h-[3rem] bg-red-100">
+                    <h1 className=""></h1>
+                </div>
+
+            </div>
         );
     }
 }
