687dd3ce3a9d879b7ffb39311b56cb58753994fb
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-05 15:33:47 +0530

    Update article.js

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-let list=require('../constants/const')
+let {articles}=require('../constants/const')
 
 /* GET all articles listing. */
 router.get('/all', function(req, res, next) {
-    res.send(list.at(0));
+    res.send(articles);
 });
 
 module.exports = router;
