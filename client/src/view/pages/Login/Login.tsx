d9d6ab42630f2740d5eab1e2e10180847e4bc427
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-06 21:55:58 +0530

    Update Login.tsx

--- client/src/view/pages/Login/Login.tsx
+++ client/src/view/pages/Login/Login.tsx
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import bgImage from "../../../images/bg.jpg"
 import axios from "axios";
+import {faHouse} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 interface LoginStates {
     isPwClicked: boolean;
@@ -49,7 +51,7 @@ export class Login extends Component<{}, LoginStates> {
                     <div className="flex flex-1 flex-col justify-center py-12 px-8
                 lg:w-[40vw] md:w-[50vw] w-screen">
                         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                            <h3 className="mx-auto h-10 w-auto">Insight</h3>
+                            <h3 className="mx-auto h-10 w-auto text-center font-extrabold">Insight</h3>
                             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                                 Sign in to your account
                             </h2>
@@ -81,7 +83,7 @@ export class Login extends Component<{}, LoginStates> {
                                             Password
                                         </label>
                                         <div className="text-sm">
-                                            <p className="active:underline font-semibold text-indigo-600 hover:text-indigo-500"
+                                            <p className="active:underline font-semibold text-secondary hover:text-tertiary cursor-pointer"
                                                onClick={this.togglePwClicked}>
                                                 Forgot password?
                                             </p>
@@ -102,7 +104,7 @@ export class Login extends Component<{}, LoginStates> {
                                 <div>
                                     <button
                                         type="button"
-                                        className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
                                         onClick={this.onLoginBtnClick}>
                                         Sign in
                                     </button>
@@ -111,7 +113,7 @@ export class Login extends Component<{}, LoginStates> {
 
                             <p className="mt-4 text-center text-sm text-gray-500 pb-3">
                                 Not a member?{' '}
-                                <Link className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                                <Link className="font-semibold leading-6 text-secondary hover:text-secondary"
                                       to="/sign-up">
                                     Sign in
                                 </Link>
@@ -123,18 +125,22 @@ export class Login extends Component<{}, LoginStates> {
                     </div>
                 </div>
 
+                <Link to={"/"} className="absolute top-0 left-0 ml-4 mt-2 text-[1em] text-tertiary rounded-full">
+                    <FontAwesomeIcon icon={faHouse} />
+                </Link>
+
                 <div className={`w-screen h-screen fixed top-0 left-0 justify-center items-center 
                 ${isPwClicked ? "hidden" : "flex"}`}>
 
                     <div className=" backdrop-blur-sm w-full h-full absolute" onClick={this.togglePwClicked}></div>
 
-                    <div className="w-2/5 h-2/3 bg-white rounded-2xl shadow-2xl px-8 z-10">
+                    <div className="xl:w-1/4 lg:w-1/3 md:w-1/3 h-2/3 sm:w-1/2 bg-white rounded-2xl shadow-2xl px-8 z-10">
                         <h2 className="mt-10 mb-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                             Reset Password
                         </h2>
                         <form className="space-y-6">
                             <div>
-                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                <label htmlFor="txtEmail" className="block text-sm font-medium leading-6 text-gray-900">
                                     Email address
                                 </label>
                                 <div className="mt-2">
@@ -174,9 +180,9 @@ export class Login extends Component<{}, LoginStates> {
                                 <div className="mt-2">
                                     <input
                                         className="pl-4 block w-full outline-0 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset sm:text-sm sm:leading-6"
-                                        id="txtPassword" name="password"
+                                        id="txtPassword" name="newPass"
                                         type="password" required
-                                        value={this.state.forgotOTP}
+                                        value={this.state.newPass}
                                         onChange={this.handleMessageInputOnChange}/>
                                 </div>
                             </div>
@@ -215,7 +221,11 @@ export class Login extends Component<{}, LoginStates> {
                 password: this.state.loginPass
             }).then((res: { data: any }) => {
                 const jsonData = res.data;
-                alert(jsonData);
+                if (jsonData==null){
+                    alert("You dont have an account")
+                }else {
+                    alert(`welcome ${jsonData.name}`);
+                }
             }).catch((error: any) => {
                 console.error('Axios Error', error);
             });
