ae5555b6cf62e22a4f7f4ea8c00adf8c98445ab8
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-07 23:03:50 +0530

    Update MainContent.tsx

--- client/src/view/common/MainContent/MainContent.tsx
+++ client/src/view/common/MainContent/MainContent.tsx
@@ -3,13 +3,13 @@ import {Route, Routes} from "react-router-dom";
 import {Home} from "../../pages/Home/Home";
 import {Contact} from "../../pages/Contact/Contact";
 import {Menu} from "../../pages/Menu/Menu";
+import {SignUp} from "../../pages/SignIn/SignUp";
 import './mainContent.css';
 
 export class MainContent extends Component {
     render() {
         return (
             <div className="min-h-[94vh] overflow-x-hidden no-scroll-bar">
-
                 <Routes>
                     <Route path="/*" Component={Home}></Route>
                     <Route path="/menu" Component={Menu}></Route>
