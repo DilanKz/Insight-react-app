7f5a4f8f7499ee075e2e10e4a4da45fbfa63fc6a
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-07 23:17:00 +0530

    Update Login.tsx

--- client/src/view/pages/Login/Login.tsx
+++ client/src/view/pages/Login/Login.tsx
@@ -59,17 +59,16 @@ export class Login extends Component<{}, LoginStates> {
 
                         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                             <form className="space-y-6">
-                                <div>
+                                <div>{/*
                                     <label htmlFor="email"
                                            className="block text-sm font-medium leading-6 text-gray-900">
                                         Email address
-                                    </label>
+                                    </label>*/}
                                     <div className="mt-2">
                                         <input
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 outline-0 focus:ring-inset pl-4 sm:text-sm sm:leading-6"
-                                            id="email"
-                                            name="loginMail"
-                                            type="email"
+                                            id="email" name="loginMail"
+                                            type="email" placeholder="email"
                                             required
                                             value={this.state.loginMail}
                                             onChange={this.handleMessageInputOnChange}/>
@@ -77,27 +76,27 @@ export class Login extends Component<{}, LoginStates> {
                                 </div>
 
                                 <div>
-                                    <div className="flex items-center justify-between">
-                                        <label htmlFor="password"
+
+                                    <div className="mt-2">
+                                        <input
+                                            className="block w-full rounded-md border-0 py-1.5 outline-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset pl-4 sm:text-sm sm:leading-6"
+                                            id="password" name="loginPass"
+                                            type="password" placeholder="password"
+                                            required
+                                            value={this.state.loginPass}
+                                            onChange={this.handleMessageInputOnChange}/>
+                                    </div>
+                                    <div className="flex items-center justify-end">
+                                        {/*<label htmlFor="password"
                                                className="block text-sm font-medium leading-6 text-gray-900">
                                             Password
-                                        </label>
+                                        </label>*/}
                                         <div className="text-sm">
-                                            <p className="active:underline font-semibold text-secondary hover:text-tertiary cursor-pointer"
+                                            <p className=" active:underline font-semibold text-secondary hover:text-tertiary cursor-pointer"
                                                onClick={this.togglePwClicked}>
                                                 Forgot password?
                                             </p>
                                         </div>
-                                    </div>
-                                    <div className="mt-2">
-                                        <input
-                                            className="block w-full rounded-md border-0 py-1.5 outline-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset pl-4 sm:text-sm sm:leading-6"
-                                            id="password"
-                                            name="loginPass"
-                                            type="password"
-                                            required
-                                            value={this.state.loginPass}
-                                            onChange={this.handleMessageInputOnChange}/>
                                     </div>
                                 </div>
 
