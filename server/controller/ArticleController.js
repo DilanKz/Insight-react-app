2653cc52e3b4439bd94d8f06984205db45a59fa8
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-10 13:14:03 +0530

    Update ArticleController.js

--- server/controller/ArticleController.js
+++ server/controller/ArticleController.js
@@ -4,6 +4,10 @@ const articleController={
 
     getAllArticles:function(req, res, next) {
         res.send(articles);
+    },
+
+    postArticle:function(req, res, next) {
+        console.log(req.body)
     }
 }
 
