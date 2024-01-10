5df48b8dde427c144216d66582c153ac45415d7a
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-10 13:13:57 +0530

    Update article.js

--- server/routes/article.js
+++ server/routes/article.js
@@ -4,5 +4,6 @@ let {articles}=require('../constants/const')
 const articleController=require('../controller/ArticleController')
 /* GET all articles listing. */
 router.get('/all', articleController.getAllArticles);
+router.post('/add', articleController.postArticle);
 
 module.exports = router;
