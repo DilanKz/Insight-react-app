b0dadb7cd45dce44743ada5bd9499dd94153644b
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-12 13:22:45 +0530

    Update MainContent.tsx

--- client/src/view/common/MainContent/MainContent.tsx
+++ client/src/view/common/MainContent/MainContent.tsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import {Home} from "../../pages/Home/Home";
 import {Post} from "../../pages/Contact/Post";
 import {Menu} from "../../pages/Menu/Menu";
+import {Article_Viewer} from "../../pages/Article_Viewer/Article_Viewer";
 import './mainContent.css';
 
 export class MainContent extends Component {
@@ -13,6 +14,10 @@ export class MainContent extends Component {
                     <Route path="/*" Component={Home}></Route>
                     <Route path="/menu" Component={Menu}></Route>
                     <Route path="/blog" Component={Post}></Route>
+
+                    <Route path="/Article" Component={Article_Viewer}></Route>
+
+
                 </Routes>
             </div>
         );
