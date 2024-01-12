49c06840e71987e9ecfe167fd9e52970299f8beb
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-12 22:45:54 +0530

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,27 +1,30 @@
-let mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const UserModel = mongoose.Schema(
-    {
-        "email": {
-            type: String,
-            require: true,
-            unique:true,
-            index: true,
-            primaryKey:true
-        },
-        "password": {
-            type: String,
-            require: true
-        },
-        "name": {
-            type: String,
-            require: true
-        },
-        "image": {
-            type: Buffer
-        }
-    }
-);
+const UserModel = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+        primaryKey: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    accountType: {
+        type: String,
+        enum: ['regular', 'author'],
+        default: 'regular',
+    },
+});
 
-const user = mongoose.model("User", UserModel);
-module.exports = user;
+const User = mongoose.model('User', UserModel);
+module.exports = User;
