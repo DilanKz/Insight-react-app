ff9ec9326277ae34c7dcb623a602bc4eb1a1be8d
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-12 22:46:15 +0530

    added password encription

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,30 +1,56 @@
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
 
-const userController={
-    getUserCredentials:async function (req, res, next) {
+const userController = {
+    getUserCredentials: async function (req, res, next) {
         const { mail, password } = req.body;
-        console.log(mail);
-        console.log(password);
-        const user = await User.findOne({ email:mail,password:password});
-        console.log(user);
+
+        const user = await User.findOne({ email:mail});
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
         res.send(user);
+
     },
-    saveUserCredentials:async function (req, res, next) {
-        const userCred = req.body;
-        console.log(userCred)
-        const user = await User.create(userCred);
-        res.send(user);
+
+    saveUserCredentials: async function (req, res, next) {
+        try {
+            const userCred = req.body;
+
+            const hashedPassword = await bcrypt.hash(userCred.password, 10);
+            const savedUser = await User.findOne({ email:userCred.email });
+
+            if (!savedUser) {
+                const user = await User.create({
+                    email: userCred.email,
+                    password: hashedPassword,
+                    name: userCred.name,
+                    image: userCred.image,
+                });
+
+                res.send(user);
+            }else {
+                return res.status(401).json({ message: 'User already exists' });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({message: 'Internal Server Error'});
+        }
     },
-    getOTP:function(req, res, next) {
+
+    getOTP: function (req, res, next) {
         let userCred = req.body;
         console.log(userCred)
         res.send('respond with a resource');
     },
-    updateCredentials:function(req, res, next) {
+
+    updateCredentials: function (req, res, next) {
         let userCred = req.body;
         console.log(userCred)
         res.send('respond with a resource');
     }
 }
 
-module.exports=userController
+module.exports = userController
