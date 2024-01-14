2d2e6ff89db75dc12ae87fc92075257b24829b64
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-14 14:15:15 +0530

    Create UserPrivacyInformation.tsx

---
+++ client/src/view/pages/UserAccountCenter/PrivacyInformation/UserPrivacyInformation.tsx
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+export class UserPrivacyInformation extends Component {
+    render() {
+        return (
+            <div className="h-full w-full bg-cyan-500">
+
+            </div>
+        );
+    }
+}
