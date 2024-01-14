a9783549b60c8daa78e86f4fd6d0373af1b34945
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-14 14:15:18 +0530

    Create UserGeneralInformation.tsx

---
+++ client/src/view/pages/UserAccountCenter/GeneralInformation/UserGeneralInformation.tsx
@@ -0,0 +1,10 @@
+import {Component} from "react";
+
+export class UserGeneralInformation extends Component {
+    render() {
+        return (
+            <div className="h-full w-full bg-green-400">
+            </div>
+        );
+    }
+}
