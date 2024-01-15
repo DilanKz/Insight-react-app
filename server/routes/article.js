1d3c3aea2930473518e4973ea76b7ef6cf1cbbbb
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-15 21:54:48 +0530

    Update article.js

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-let {articles}=require('../constants/const')
 const articleController=require('../controller/ArticleController')
 /* GET all articles listing. */
 router.get('/all', articleController.getAllArticles);
 router.post('/add', articleController.postArticle);
+router.get('/from/:id', articleController.getAllFromAuthor);
 
 module.exports = router;
