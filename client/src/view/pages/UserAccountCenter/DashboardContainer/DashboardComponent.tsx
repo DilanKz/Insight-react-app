a5ee9e0aacecd64cabc2ea5bc8837efad1ec8094
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-15 12:29:32 +0530

    Update DashboardComponent.tsx

--- client/src/view/pages/UserAccountCenter/DashboardContainer/DashboardComponent.tsx
+++ client/src/view/pages/UserAccountCenter/DashboardContainer/DashboardComponent.tsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import {UserGeneralInformation} from "../GeneralInformation/UserGeneralInformation";
 import {UserPrivacyInformation} from "../PrivacyInformation/UserPrivacyInformation";
 import {ArticleList} from "../ArticleList/ArticleList";
+import {PostArticle} from "../PostArticle/PostArticle";
 
 export class DashboardComponent extends Component {
     render() {
@@ -14,6 +15,7 @@ export class DashboardComponent extends Component {
                     <Route path="general" Component={UserGeneralInformation}></Route>
                     <Route path="privacy" Component={UserPrivacyInformation}></Route>
                     <Route path="list" Component={ArticleList}></Route>
+                    <Route path="post" Component={PostArticle}></Route>
 
                 </Routes>
 
