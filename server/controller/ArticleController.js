9468691ec240749bca0d3b4ca70b2ca8740970e2
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-15 19:53:46 +0530

    Update ArticleController.js

--- server/controller/ArticleController.js
+++ server/controller/ArticleController.js
@@ -1,14 +1,24 @@
 const {articles} = require("../constants/const");
+const Article = require("../models/ArticleModel");
 
-const articleController={
+const articleController = {
 
-    getAllArticles:function(req, res, next) {
+    getAllArticles: function (req, res, next) {
         res.send(articles);
     },
 
-    postArticle:function(req, res, next) {
-        console.log(req.body);
-        res.send("Article has saved successfully");
+    postArticle: async function (req, res, next) {
+        const article = req.body;
+
+        const user = await Article.create({
+            author: article.id,
+            title: article.title,
+            body: article.body,
+            tags: article.tags,
+            image: article.image,
+        });
+
+        res.send(user);
     }
 }
 
