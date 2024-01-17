d103dbd99728d65dda4f904cf1c14d290ad2350c
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-17 22:04:13 +0530

    Update CategoryContainer.tsx

--- client/src/view/pages/ArticlePage/Category/CategoryContainer.tsx
+++ client/src/view/pages/ArticlePage/Category/CategoryContainer.tsx
@@ -3,7 +3,20 @@ import {Component} from "react";
 export class CategoryContainer extends Component {
     render() {
         return (
-            <></>
+            <div className="w-full px-16 mb-10">
+                <h1 className={"text-gray-800 text-4xl my-10"}>Explore</h1>
+                <div className="grid grid-cols-5 gap-4">
+                    <div className="col-span-2 h-52 bg-gray-300 rounded-xl"></div>
+                    <div className="col-span-1 h-52 bg-gray-300 rounded-xl"></div>
+                    <div className="col-span-1 h-52 bg-gray-300 rounded-xl"></div>
+                    <div className="col-span-1 h-52 bg-gray-300 rounded-xl"></div>
+                    <div className="col-span-1 h-52 bg-gray-300 rounded-xl"></div>
+                    <div className="col-span-1 h-52 bg-gray-300 rounded-xl"></div>
+                    <div className="col-span-1 h-52 bg-gray-300 rounded-xl"></div>
+                    <div className="col-span-2 h-52 bg-gray-300 rounded-xl"></div>
+                </div>
+
+            </div>
         );
     }
 }
