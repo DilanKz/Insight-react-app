555f65602d741fc195bd4847a973efec5a728f0f
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-17 16:00:26 +0530

    Update RowSkeleton.tsx

--- client/src/view/pages/UserAccountCenter/ArticleList/RowSkeleton.tsx
+++ client/src/view/pages/UserAccountCenter/ArticleList/RowSkeleton.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import img from "../../../../images/selectImage.jpg";
+import {faImage} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export class RowSkeleton extends Component {
     render() {
@@ -8,18 +10,18 @@ export class RowSkeleton extends Component {
 
                 <td className="p-4 border-b border-blue-gray-50 w-24 ">
                     <div className="flex items-center gap-3">
-                        <div className="inline-block relative object-center bg-gray-300 animate-pulse my-2
-                         w-12 h-8 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1"
-
-                        />
+                        <div className="justify-center items-center flex relative bg-gray-300 animate-pulse my-2
+                         w-12 h-8 rounded-md border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1">
+                            <FontAwesomeIcon className="text-gray-400" icon={faImage} />
+                        </div>
                     </div>
 
                 </td>
 
                 <td className="p-4 border-b border-blue-gray-50 w-[48%] ">
-                    <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal px-4">
+                    <div className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal px-4">
                         <p className="w-11/12 h-4 bg-gray-300 animate-pulse rounded-full"></p>
-                    </p>
+                    </div>
                 </td>
 
                 <td className="p-4 border-b border-blue-gray-50">
