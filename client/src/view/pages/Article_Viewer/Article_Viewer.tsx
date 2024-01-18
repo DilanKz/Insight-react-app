30dbcb78d0ef73c3c05454b24cee2f31547daf31
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-18 21:52:54 +0530

    Update Article_Viewer.tsx

--- client/src/view/pages/Article_Viewer/Article_Viewer.tsx
+++ client/src/view/pages/Article_Viewer/Article_Viewer.tsx
@@ -1,18 +1,17 @@
-import {Component} from "react";
-import {data} from "autoprefixer";
+import React, {Component} from "react";
+import "./Viewer.css"
 
+export class Article_Viewer extends Component<{}, {}> {
 
-export class Article_Viewer extends Component<{} ,{}> {
+    private Article: any;
 
-    private Article:any;
-
-    constructor(props:{} ) {
+    constructor(props: {}) {
         super(props);
 
         let item = localStorage.getItem('articleData');
 
-        if (item){
-            this.Article=JSON.parse(item);
+        if (item) {
+            this.Article = JSON.parse(item);
         }
         console.log(item)
     }
@@ -20,11 +19,41 @@ export class Article_Viewer extends Component<{} ,{}> {
     render() {
 
         return (
-            <div className="pt-16">
+            <div className="pt-28 px-16">
 
-                <h3 className="pl-10 pt-16">{this.Article.title}</h3>
-                <h3 className="pl-10">{this.Article.tags}</h3>
-                <h3 className="pl-10">{this.Article.body}</h3>
+                <div className="flex flex-wrap mb-6">
+
+                    <div className="pr-4 md:w-[65%] w-[90%]">
+                        <h3 className="xl:text-9xl lg:text-7xl md:text-6xl text-4xl font-serifDisplay">{this.Article.title}</h3>
+
+                        <div className="p-2 pl-0 flex md:gap-4 gap-1 flex-wrap ">
+                            {
+                                this.Article.tags.map((tag: any) => (
+
+                                    <span key={tag} className="bg-gray-200 flex justify-center items-center p-2 py-0.5
+                                    md:text-xl text-sm rounded-full font-light hover:cursor-pointer">
+                                        {tag}
+                                    </span>
+
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    <div className="md:w-[30%] w-[35rem] h-[26rem] rounded-2xl">
+
+                        <div className="md:w-full h-full bg-gray-200 rounded-2xl relative transform">
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div
+                    dangerouslySetInnerHTML={{__html: this.Article.body}}
+                    className="prose custom-content text-2xl"
+                ></div>
+
 
             </div>
         );
