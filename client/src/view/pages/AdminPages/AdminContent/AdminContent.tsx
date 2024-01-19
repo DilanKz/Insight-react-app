f1690a05d24ddba687dd6552f2120bcd5e9cbac4
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-19 18:26:25 +0530

    Create AdminContent.tsx

---
+++ client/src/view/pages/AdminPages/AdminContent/AdminContent.tsx
@@ -0,0 +1,21 @@
+import {Component} from "react";
+import {Route, Routes} from "react-router-dom";
+import {AdminDashboard} from "../AdminDashboard/AdminDashboard";
+import {AuthorRequests} from "../AuthorRequests/AuthorRequests";
+
+export class AdminContent extends Component {
+    render() {
+        return (
+            <div className="w-full">
+                <Routes>
+
+                    <Route Component={AdminDashboard} path="dashboard"></Route>
+                    <Route Component={AuthorRequests} path="requests"></Route>
+                    <Route Component={AdminDashboard} path="addTag"></Route>
+                    <Route Component={AdminDashboard} path="addTag"></Route>
+
+                </Routes>
+            </div>
+        );
+    }
+}
