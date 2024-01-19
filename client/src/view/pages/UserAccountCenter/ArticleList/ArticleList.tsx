5ca6edf2ba548753ae20dad01b5c220d2beb01da
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-19 18:27:00 +0530

    Update ArticleList.tsx

--- client/src/view/pages/UserAccountCenter/ArticleList/ArticleList.tsx
+++ client/src/view/pages/UserAccountCenter/ArticleList/ArticleList.tsx
@@ -69,45 +69,6 @@ export class ArticleList extends Component<{}, ArticleListStates> {
         return (
             <div className="h-full w-full relative">
 
-                {/*<table classNameNameName="w-full">
-                    <thead>
-
-                    <tr>
-                        <th scope="col" classNameNameName="py-3.5 pl-4 pr-3 text-left text-sm font-bold text-gray-800">Name</th>
-                        <th scope="col" classNameNameName="px-3 py-3.5 text-left text-sm font-bold text-gray-800">Contact</th>
-                        <th scope="col" classNameNameName="relative py-3.5 pl-3 pr-4"><span classNameNameName="hidden">Edit</span></th>
-                    </tr>
-
-                    </thead>
-                    <tbody classNameNameName="">
-                    <tr>
-                        <td classNameNameName="whitespace-nowrap py-3 pl-4 pr-3 text-sm">
-                            <div classNameNameName="flex items-center">
-                                <div classNameNameName="w-11 h-11 flex-shrink-0">
-                                    <img classNameNameName="h-11 rounded-full w-11"
-                                         src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-                                         alt=""/>
-                                </div>
-
-                                <div classNameNameName="ml-4">
-                                    <div classNameNameName="text-gray-900 font-medium">Tharindu Dilan</div>
-                                    <div classNameNameName="mt-1 text-gray-400">Dilanmallawarachchi@gmail.com</div>
-                                </div>
-                            </div>
-                        </td>
-
-                        <td classNameNameName="px-3 py-5 text-sm text-gray-800">
-                            <div classNameNameName="text-gray-800">07X XXXX XXX</div>
-                        </td>
-
-                        <td classNameNameName="px-3 py-5 text-sm text-gray-800">
-                            <FontAwesomeIcon classNameNameName="pr-5 cursor-pointer text-green-700" icon={faCheck}></FontAwesomeIcon>
-                            <FontAwesomeIcon classNameNameName="pl-4 cursor-pointer text-red-700" icon={faX}></FontAwesomeIcon>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>*/}
-
                 <div className="w-full flex justify-between items-end">
                     <h2 className="text-2xl font-bold sm:text-xl pt-8 ps-3 w-max">Posted Articles</h2>
 
