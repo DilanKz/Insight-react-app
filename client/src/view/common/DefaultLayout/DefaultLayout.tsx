da92b88c108bfbf9b272aaddba3bbb473f5ce8b5
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-20 19:26:40 +0530

    Update DefaultLayout.tsx

--- client/src/view/common/DefaultLayout/DefaultLayout.tsx
+++ client/src/view/common/DefaultLayout/DefaultLayout.tsx
@@ -1,10 +1,32 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import {Navbar} from "../Navbar/Navbar";
 import {MainContent} from "../MainContent/MainContent";
 import {Footer} from "../Footer/Footer";
+import {Navigate} from "react-router-dom";
 
 export class DefaultLayout extends Component {
+
+    constructor(props:{}) {
+        super(props);
+
+        let item:any = localStorage.getItem('insightUser');
+        let loggedUser = JSON.parse(item);
+
+        this.state = {
+            user:loggedUser
+        }
+
+    }
+
+
     render() {
+        //@ts-ignore
+        const {user}= this.state;
+
+        if (user !=null && user.accountType === 'admin') {
+            return <Navigate to='/admin/dashboard' replace={true}/>;
+        }
+
         return (
             <>
                 <Navbar/>
