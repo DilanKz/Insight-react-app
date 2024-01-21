2c0f1263ea89602a446377bea249fa213b31cd76
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-21 18:16:26 +0530

    Update AdminContent.tsx

--- client/src/view/pages/AdminPages/AdminContent/AdminContent.tsx
+++ client/src/view/pages/AdminPages/AdminContent/AdminContent.tsx
@@ -2,6 +2,7 @@ import {Component} from "react";
 import {Route, Routes} from "react-router-dom";
 import {AdminDashboard} from "../AdminDashboard/AdminDashboard";
 import {AuthorRequests} from "../AuthorRequests/AuthorRequests";
+import {ArticleRequests} from "../ArticleRequests";
 
 export class AdminContent extends Component {
     render() {
@@ -11,7 +12,7 @@ export class AdminContent extends Component {
 
                     <Route Component={AdminDashboard} path="dashboard"></Route>
                     <Route Component={AuthorRequests} path="requests"></Route>
-                    <Route Component={AdminDashboard} path="delete"></Route>
+                    <Route Component={ArticleRequests} path="delete"></Route>
                     <Route Component={AdminDashboard} path="add"></Route>
 
                 </Routes>
