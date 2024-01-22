3a99fad95e0a50e3fb9ef324d2c958df05912da7
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-22 20:17:37 +0530

    Update AdminContent.tsx

--- client/src/view/pages/AdminPages/AdminContent/AdminContent.tsx
+++ client/src/view/pages/AdminPages/AdminContent/AdminContent.tsx
@@ -3,17 +3,18 @@ import {Route, Routes} from "react-router-dom";
 import {AdminDashboard} from "../AdminDashboard/AdminDashboard";
 import {AuthorRequests} from "../AuthorRequests/AuthorRequests";
 import {ArticleRequests} from "../ArticleRequests/ArticleRequests";
+import {TagManager} from "../TagManager/TagManager";
 
 export class AdminContent extends Component {
     render() {
         return (
-            <div className="w-full">
+            <div className="w-full h-screen">
                 <Routes>
 
                     <Route Component={AdminDashboard} path="dashboard"></Route>
                     <Route Component={AuthorRequests} path="requests"></Route>
                     <Route Component={ArticleRequests} path="delete"></Route>
-                    <Route Component={AdminDashboard} path="add"></Route>
+                    {/*<Route Component={TagManager} path="add"></Route>*/}
 
                 </Routes>
             </div>
