86d9d56ca9c7f276e168e7585c39bc06e0dc3e60
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-22 22:53:00 +0530

    Update ArticleContainerSkelaton.tsx

--- client/src/view/pages/ArticlePage/ArticleContainer/ArticleContainerSkelaton.tsx
+++ client/src/view/pages/ArticlePage/ArticleContainer/ArticleContainerSkelaton.tsx
@@ -1,4 +1,5 @@
-import {Component} from "react";
+import React, {Component} from "react";
+import {ArticleSkeleton} from "../../../common/Article/ArticleSkeleton";
 
 export class ArticleContainerSkeleton extends Component {
     render() {
@@ -7,7 +8,7 @@ export class ArticleContainerSkeleton extends Component {
 
                 <div className="w-full flex justify-between flex-wrap">
 
-                    <div className="md:w-[55%] w-full h-60 bg-gray-300 object-cover bg-center rounded-xl animate-pulse">
+                    <div className="md:w-[55%] w-full h-80 bg-gray-300 object-cover bg-center rounded-xl animate-pulse">
 
                     </div>
 
@@ -29,20 +30,12 @@ export class ArticleContainerSkeleton extends Component {
                 </div>
                 <h1 className={"text-gray-800 text-4xl my-10 bg-gray-300 h-8 w-1/4 rounded-md animate-pulse"}></h1>
                 <div
-                    className="flex md:justify-between justify-center w-full py-4 h-max mt-8 mb-8 flex-wrap gap-y-4 gap-x-2">
+                    className="flex md:justify-start justify-center w-full py-4 h-max mt-8 mb-8 flex-wrap gap-y-4">
 
-                    <div className="w-72 h-96 bg-gray-300 rounded-xl animate-pulse">
-
-                    </div>
-                    <div className="w-72 h-96 bg-gray-300 rounded-xl animate-pulse">
-
-                    </div>
-                    <div className="w-72 h-96 bg-gray-300 rounded-xl animate-pulse">
-
-                    </div>
-                    <div className="w-72 h-96 bg-gray-300 rounded-xl animate-pulse">
-
-                    </div>
+                    <ArticleSkeleton/>
+                    <ArticleSkeleton/>
+                    <ArticleSkeleton/>
+                    <ArticleSkeleton/>
 
                 </div>
 
